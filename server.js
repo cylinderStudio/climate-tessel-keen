ab1486e5ac7c3505bc1bcfd8c729e7963cdce280
Gregory Martin <gregory@cylinderstudio.com>
2014-12-17 23:01:05 -0800

    Refactored to include some programmatic control of the wifi chip and/or handle wifi-related issues

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var KEEN_WRITE_KEY = config.keen.write_key;
 var WEATHER_APP_KEY = config.weather.app_key;
 var CHECK_TEMPS_INTERVAL = 300000;	// 300000 milliseconds = 5 minutes
 
+var wifi = require('wifi-cc3000');
 var tessel = require('tessel');
 var climatelib = require('climate-si7020');
 var climate = climatelib.use(tessel.port['A']);
@@ -12,6 +13,26 @@ var request = require('request');
 
 var Keen = require('keen.io');
 var keen_client = Keen.configure({projectId: KEEN_PROJECT_ID, writeKey: KEEN_WRITE_KEY});
+
+wifi.reset(function() {		
+	// resetting wifi to clear state from any previous operations that didn't finish
+	console.log('wifi reset');
+
+	wifi.on('connect', function() {
+		console.log('wifi connected');
+	});
+});
+
+wifi.on('disconnect', function(data) {
+	console.log('wifi disconnected:');
+	console.log(data);
+	setTimeout(function() { wifi.connect(); }, 20000);
+});
+
+wifi.on('error', function(err) {
+	console.log('wifi emitted error:');
+	console.log(err);
+});
 
 climate.on('ready', function() {
 	console.log('Connected to climate module');
@@ -22,24 +43,28 @@ climate.on('ready', function() {
 				var room_temp = Math.round(temp);
 				var outside_temp;
 
-				request.get('http://api.wunderground.com/api/' + WEATHER_APP_KEY + '/conditions/q/WA/Seattle.json', function(error, response, body) {
-					if (!error && response.statusCode == 200) {
-						outside_temp = Math.round(JSON.parse(body).current_observation.temp_f);
+				if (wifi.isConnected()) {
+					request.get('http://api.wunderground.com/api/' + WEATHER_APP_KEY + '/conditions/q/WA/Seattle.json', function(error, response, body) {
+						if (!error && response.statusCode == 200) {
+							outside_temp = Math.round(JSON.parse(body).current_observation.temp_f);
 
-						keen_client.addEvents({
-							"outside temperatures": [{"temperature": outside_temp}],
-							"inside temperatures": [{"temperature": room_temp}]
-						}, function(k_err) {
-							if (k_err){
-								console.log("Error posting to Keen event:");
-								console.log(k_err)
-							}
-						});
-					};
-				}).on('error', function(e) {
-					console.log('http module error');
-					console.log(e);
-				});
+							keen_client.addEvents({
+								"outside temperatures": [{"temperature": outside_temp}],
+								"inside temperatures": [{"temperature": room_temp}]
+							}, function(k_err) {
+								if (k_err){
+									console.log("Error posting to Keen event:");
+									console.log(k_err)
+								}
+							});
+						};
+					}).on('error', function(e) {
+						console.log('http module error');
+						console.log(e);
+					});
+				} else {
+					console.log('Did not post to Keen - wifi disconnected');
+				}
 			} else {
 				console.log('Error reading temperature from climate module');
 				console.log(err);
