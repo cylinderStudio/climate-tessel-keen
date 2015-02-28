d98353c87c5376e0b26a54736d11ab8baee1dade
Gregory Martin <gregory@cylinderstudio.com>
2015-02-27 22:34:31 -0800

    Ditched the request module in favor of plain http requests. The module was no longer working with Tessel.

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ var wifi = require('wifi-cc3000');								// this is one of the built in tessel 
 var tessel = require('tessel');
 var climatelib = require('climate-si7020');
 var climate = climatelib.use(tessel.port['A']);
-var request = require('request');
-
+var http = require('http');
 var Keen = require('keen.io');
 var keen_client = Keen.configure({projectId: KEEN_PROJECT_ID, writeKey: KEEN_WRITE_KEY});
+//var keen_client = new Keen({projectId: KEEN_PROJECT_ID, writeKey: KEEN_WRITE_KEY});
 
 wifi.reset(function() {		
 	// resetting wifi to clear state from any previous operations that didn't finish
@@ -44,23 +44,30 @@ climate.on('ready', function() {
 				var outside_temp;
 
 				if (wifi.isConnected()) {
-					request.get('http://api.wunderground.com/api/' + WEATHER_APP_KEY + '/conditions/q/WA/Seattle.json', function(error, response, body) {
-						if (!error && response.statusCode == 200) {
-							outside_temp = Math.round(JSON.parse(body).current_observation.temp_f);
+
+					http.get('http://api.wunderground.com/api/' + WEATHER_APP_KEY + '/conditions/q/WA/Seattle.json', function(response) {
+		        var body = '';
+		        response.on('data', function(d) {
+		        	body += d;
+		        });
+		        response.on('end', function() {
+	            outside_temp = Math.round(JSON.parse(body).current_observation.temp_f);
 
 							keen_client.addEvents({
-								"outside temperatures": [{"temperature": outside_temp}],
-								"inside temperatures": [{"temperature": room_temp}]
+								'outside temperatures': [{'temperature': outside_temp}],
+								'inside temperatures': [{'temperature': room_temp}]
 							}, function(k_err) {
 								if (k_err){
-									console.log("Error posting to Keen event:");
+									console.log('Error posting Keen event:');
 									console.log(k_err)
+								} else {
+									console.log('Keen event posted');
 								}
 							});
-						};
+		        });
 					}).on('error', function(e) {
-						console.log('http module error');
-						console.log(e);
+					  console.log('http module error:');
+						console.log(e)
 					});
 				} else {
 					console.log('Did not post to Keen - wifi disconnected');
